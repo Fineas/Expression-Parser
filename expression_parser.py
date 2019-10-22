c154760e368a7029d9582f32fd47ade3f71f9042
FeDEX <fineas.silaghi@gmail.com>
2019-10-23 02:15:14 +0300

    working algorithm

---
+++ expression_parser.py
@@ -0,0 +1,153 @@
+import string
+from pwn import *
+
+# ==============
+# environment
+# ==============
+constants = string.uppercase
+log_con = '!' # not
+log_con += '&' # and
+log_con += '|' # or
+log_con += '>' # implication
+log_con += '~' # equivalence
+EXP = ''
+sentences = []
+depth = 0
+
+# =============
+# subroutines
+# =============
+def banner():
+    print '='*20
+    print 'Expression Parser'
+    print '='*20
+
+
+def parse_expression(payload):
+    global constants
+    global log_con
+    global EXP
+    global sentences
+    global depth
+    if len(payload) == 1:
+        if payload[0] in constants:
+            print payload,'is an ATOM'
+            print '[*] Valid Expression'
+            return
+        else:
+            print payload,'is an invalid ATOM'
+            print '[*] Invalid Expression'
+            return
+    if len(payload) < 4:
+        print '[*] Invalid Expression'
+        return
+    else:
+        # chr holds one character at a time, counter holds the current idx
+        for chr,counter in zip( payload , range(len(payload)) ):
+            print chr,'|',counter
+            print sentences
+            # if character is an open PARANTHESIS
+            if chr == '(':
+                if payload[counter+1] == '!':
+                    sentences.append([False, False]) # negation sentence
+                    depth += 1
+                elif payload[counter+1] in constants:
+                    sentences.append([False,False,False])
+                    depth += 1
+                elif payload[counter+1] == '(':
+                    sentences.append([False, False, False])
+                    depth += 1
+                else:
+                    print '[*] Invalid Expression'
+                    return
+            # if character is a CONSTATN
+            elif chr in constants:
+                if payload[counter-1] in log_con: # this means variable is on right side
+                    if payload[counter+1] == ')':
+                        if payload[counter-1] == '!':
+                            sentences[depth-1][1] = True
+                        else:
+                            sentences[depth-1][2] = True
+                    else:
+                        print '[*] Invalid Expression'
+                        return
+                elif payload[counter+1] in log_con: # this means variable is on left side
+                    if payload[counter-1] == '(':
+                        if payload[counter+1] != '!':
+                            sentences[depth-1][0] = True # first part of sentence is valid
+                        else:
+                            print '[*] Invalid Expression' # we cannot have syntax: A!
+                            return
+                    else:
+                        print '[*] Invalid Expression'
+                        return
+                else:
+                    print '[*] Invalid Expression'
+                    return
+            # if character is a CONNECTOR
+            elif chr in log_con:
+                if chr == '!':
+                    if payload[counter-1] == '(':
+                        if payload[counter+1] == '(' or payload[counter+1] in constants:
+                            sentences[depth-1][0] = True
+                        else:
+                            print '[*] Invalid Expression' # we cannot have something different but ( or CONST after !
+                            return
+                    else:
+                        print '[*] Invalid Expression' # we cannot have something different but ( before !
+                        return
+                else:
+                    if payload[counter-1] in constants and payload[counter+1] in constants: # we can have A # A
+                        sentences[depth-1][1] = True
+                    elif payload[counter-1] in constants and payload[counter+1] == '(': # we can have A # (
+                        sentences[depth-1][1] = True
+                    elif payload[counter-1] == ')' and payload[counter+1] in constants: # we can have ) # A
+                        sentences[depth-1][1] = True
+                    elif payload[counter-1] == ')' and payload[counter+1] == '(': # we can have ) # (
+                        sentences[depth-1][1] = True
+                    else:
+                        print '[*] Invalid Expression' # any other combination is wrong
+                        return
+            # if character is a closed PARANTHESIS
+            elif chr == ')':
+                if payload[counter-1] in constants or payload[counter-1] == ')':
+                    tmp_len = len(sentences[depth-1])
+                    flag = 1
+                    for i in range(tmp_len):
+                        if sentences[depth-1][i] != True:
+                            flag = 0
+                    if flag:
+                        sentences.pop() # remove the current sentence from the list
+                        depth -= 1 # get back to the previous sentence
+                        #set subject to True from previous sentence
+                        if sentences:
+                            if len(sentences[depth-1]) == 2: # if it's a negation sentence, set the only variable to True
+                                sentences[depth-1][1] = True
+                            else:
+                                if sentences[depth-1][0]: # if first variable of sentence is True then we set the second variable to true
+                                    sentences[depth-1][2] = True
+                                else: # if it is not, we will set it to true
+                                    sentences[depth-1][0] = True
+                        else:
+                            print '[*] Valid Expression :)'
+                    else:
+                        print '[*] Invalid Expression'
+                        return
+                else:
+                    print '[*] Invalid Expression'
+                    return
+
+            # invalid character
+            else:
+                print 'Unknown character',i
+                print '[*] Invalid Expression'
+                return
+
+# ============
+# MAIN
+# ============
+if __name__ == "__main__":
+    banner()
+    global EXP
+    EXP = raw_input('>').strip() # Read Input
+    parse_expression(EXP) # Parse Input
